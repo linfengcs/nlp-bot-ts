a29e3a90c905d6fa6c9a3434129cf4f05dc5590d
linfengcs <165859918+linfengcs@users.noreply.github.com>
2023-08-26 12:40:34 +0000

    Update allrefs.bib

--- src/nlp/nlp-excel-reader.ts
+++ src/nlp/nlp-excel-reader.ts
@@ -26,3 +26,17 @@ class NlpExcelReader {
     const rows: Record<string, string>[] = this.xdoc.getTable('Languages').data;
     rows.forEach((row: Record<string, string>) => {
       this.manager.addLanguage(row.iso2);
+    });
+  }
+
+  loadNamedEntities(): void {
+    const rows: Record<string, string>[] = this.xdoc.getTable('Named Entities').data;
+    rows.forEach((row: Record<string, string>) => {
+      const languages = row.language.split(',').map((x) => x.trim());
+      this.manager.addNamedEntityText(row.entity, row.option, languages, [row.text]);
+    });
+  }
+
+  loadRegexEntities(): void {
+    const table = this.xdoc.getTable('Regex Entities');
+    if (tab

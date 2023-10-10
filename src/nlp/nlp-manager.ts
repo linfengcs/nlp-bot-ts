bf910e989bf02d2546fcd942ea210885bab47820
linfengcs <165859918+linfengcs@users.noreply.github.com>
2023-10-10 19:47:16 +0000

    add screenshot

--- src/nlp/nlp-manager.ts
+++ src/nlp/nlp-manager.ts
@@ -141,4 +141,16 @@ class NlpManager {
     return this.sentimentManager.translate(sentiment.sentiment);
   }
 
-  addNamedEntityText(entityName: string, optionName: string, languages: string[], texts: string[]): voi
+  addNamedEntityText(entityName: string, optionName: string, languages: string[], texts: string[]): void {
+    return this.nlp.addNerRuleOptionTexts(languages, entityName, optionName, texts);
+  }
+
+  removeNamedEntityText(entityName: string, optionName: string, languages: string[], texts: string[]): void {
+    return this.nlp.removeNerRuleOptionTexts(languages, entityName, optionName, texts);
+  }
+
+  addRegexEntity(entityName: string, languages: string[], regex: string): void {
+    return this.nlp.addNerRegexRule(languages, entityName, regex);
+  }
+
+  addBetweenCondition(locale: string, n

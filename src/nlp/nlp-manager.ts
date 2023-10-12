05dc40f7d8bbd56f271e799f7363bd7fb7f92aff
linfengcs <165859918+linfengcs@users.noreply.github.com>
2023-10-12 08:01:50 +0000

    File deleted.

--- src/nlp/nlp-manager.ts
+++ src/nlp/nlp-manager.ts
@@ -153,4 +153,13 @@ class NlpManager {
     return this.nlp.addNerRegexRule(languages, entityName, regex);
   }
 
-  addBetweenCondition(locale: string, n
+  addBetweenCondition(locale: string, name: string, left: string, right: string, opts?: any): void {
+    return this.nlp.addNerBetweenCondition(locale, name, left, right, opts);
+  }
+
+  addPositionCondition(locale: string, name: string, position: string, words: string[], opts?: any): void {
+    return this.nlp.addNerPositionCondition(locale, name, position, words, opts);
+  }
+
+  addAfterCondition(locale: string, name: string, words: string[], opts?: any): void {
+    return this.nlp.addNerAfterCondition(locale, name, words, opts);

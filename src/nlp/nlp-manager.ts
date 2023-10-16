64395061298fc207dfeebb3464bdd97a3b80d9d1
linfengcs <165859918+linfengcs@users.noreply.github.com>
2023-10-16 17:29:25 +0000

    button changes

--- src/nlp/nlp-manager.ts
+++ src/nlp/nlp-manager.ts
@@ -175,3 +175,15 @@ class NlpManager {
 
   addBeforeCondition(locale: string, name: string, words: string[], opts?: any): void {
     return this.nlp.addNerBeforeCondition(locale, name, words, opts);
+  }
+
+  addBeforeFirstCondition(locale: string, name: string, words: string[], opts?: any): void {
+    return this.nlp.addNerBeforeFirstCondition(locale, name, words, opts);
+  }
+
+  addBeforeLastCondition(locale: string, name: string, words: string[], opts?: any): void {
+    return this.nlp.addNerBeforeLastCondition(locale, name, words, opts);
+  }
+
+  describeLanguage(locale: string, name: string): void {
+    return this.nlp.describeLanguage(locale, name);

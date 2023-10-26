4e68609b762acc3a1a3950995db9a48d7a8c20e5
linfengcs <165859918+linfengcs@users.noreply.github.com>
2023-10-26 18:56:53 +0000

    Added time

--- src/nlp/nlp-manager.ts
+++ src/nlp/nlp-manager.ts
@@ -217,4 +217,19 @@ class NlpManager {
   }
 
   fromObj(obj: any): any {
-    return this.nlp.fromJS
+    return this.nlp.fromJSON(obj);
+  }
+
+  /**
+   * Export NLP manager information as a string.
+   * @param {Boolean} minified If true, the returned JSON will have no spacing or indentation.
+   * @returns {String} NLP manager information as a JSON string.
+   */
+  export(minified = false): string {
+    const clone = this.toObj();
+    return minified ? JSON.stringify(clone) : JSON.stringify(clone, null, 2);
+  }
+
+  /**
+   * Load NLP manager information from a string.
+   * @param {Stri

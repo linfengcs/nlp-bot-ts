074c83b5e14db5d2a41cda6ae1e5e6d62188954d
linfengcs <165859918+linfengcs@users.noreply.github.com>
2024-01-15 13:08:01 +0000

    webpack config

--- src/nlu/brain-nlu.ts
+++ src/nlu/brain-nlu.ts
@@ -36,4 +36,23 @@ class BrainNLU {
     if (!this.settings.container) {
       this.settings.container = containerBootstrap();
     }
-    
+    this.container = this.settings.container;
+    this.container.use(LangAll);
+    if (!this.settings.l) {
+      this.nlu = new NluNeural({
+        locale: this.settings.locale || this.settings.language || 'en',
+      });
+    }
+    this.corpus = [];
+  }
+
+  add(utterance: string, intent: string) {
+    this.corpus.push({ utterance, intent });
+  }
+
+  train() {
+    return this.nlu?.train(this.corpus, this.settings);
+  }
+
+  async getClassifications(utterance: string) {
+    const

89efb7a339f91a341c1ec8baa7a86e10c5a5ad81
linfengcs <165859918+linfengcs@users.noreply.github.com>
2024-01-15 18:13:22 +0000

    Reformat README

--- src/nlu/brain-nlu.ts
+++ src/nlu/brain-nlu.ts
@@ -55,4 +55,14 @@ class BrainNLU {
   }
 
   async getClassifications(utterance: string) {
-    const
+    const result = await this.nlu?.process(utterance);
+    return result?.classifications.sort((a, b) => b.score - a.score);
+  }
+
+  async getBestClassification(utterance: string) {
+    const result = await this.getClassifications(utterance);
+    return result?.[0];
+  }
+}
+
+export default BrainNLU;
